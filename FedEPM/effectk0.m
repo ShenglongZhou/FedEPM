d8911e681058e5e4e54ebdbff42bd270d16aef9e
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-19 23:18:16 +0000

    Update effectk0.m

--- FedEPM/effectk0.m
+++ FedEPM/effectk0.m
@@ -1,16 +1,15 @@
 clc; clear; close all;
 addpath(genpath(pwd));
-dat     = load('adult.mat'); 
-lab     = load('adultclass.mat'); 
-A       = Normalization(dat.X,3); 
-b       = lab.y; 
-[d,n]   = size(A);  
 
-I       = randperm(d);
-A       = A(I,:);  % shuffle samples
-b       = b(I,:);  
+A     = load('adult.mat').X; 
+b     = load('adultclass.mat').y; 
+A     = Normalization(A,3);  
+[d,n] = size(A);  
+I     = randperm(d);
+A     = A(I,:);  % shuffle samples
+b     = b(I,:);    
 
-m       =  100; 
+m     =  100; 
 while 1
     idx = unique([randperm(d-2,m-1)+1 d]);
     di  = (idx-[0 idx(1:end-1)])'; 
@@ -18,10 +17,8 @@ while 1
 end
  
 pars.noise   = 0.1;    
-pars.rho     = 0.5; 
-pars.eta     = 2e-5*(pars.rho+0.1)*(m/100+0.5);
-pars.lam     = pars.eta /2;
-k0           = [4 8 12 16 20];
+pars.rho     = 0.5;  
+k0           = 4:4:20;
 for i        = 1:nnz(k0) 
     out{i}   = FedEPM(di,n,A,b,k0(i),pars);
 end
